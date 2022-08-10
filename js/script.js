aba852bb8cc8c0102e2551851e8fe76740482e74
teke <mutau85@gmail.com>
2022-08-10 07:50:29 +0200

    Change Font Family

--- js/script.js
+++ js/script.js
@@ -3,7 +3,18 @@ const inputBtns = document.querySelectorAll('button')
 const clearBtn = document.getElementById('clear-btn')
 
 function sendNumberValue(number) {
-  calculatorDisplay.textContent = number
+  //if the current display value is 0, replace it, if not add number
+  const displayValue = calculatorDisplay.textContent
+  calculatorDisplay.textContent =
+    displayValue === '0' ? number : displayValue + number
+}
+
+function addDecimal() {
+  // If no decimal, add one
+
+  if (!calculatorDisplay.textContent.includes('.')) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`
+  }
 }
 
 // Add Event Listeners for numbers, operators, decimal buttons
@@ -13,6 +24,16 @@ inputBtns.forEach((inputBtn) => {
   } else if (inputBtn.classList.contains('operator')) {
     inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value))
   } else if (inputBtn.classList.contains('decimal')) {
-    inputBtn.addEventListener('click', () => sendNumberValue())
+    inputBtn.addEventListener('click', () => addDecimal())
   }
 })
+
+// Reset display
+function resetAll() {
+  calculatorDisplay.textContent = '0'
+}
+
+// Event Listener
+clearBtn.addEventListener('click', resetAll)
+
+//
